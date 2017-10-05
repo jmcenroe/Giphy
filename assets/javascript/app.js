ee51046f97b7ae23d7971a0b01e70e9a4a1a097b
Jarrod <jwmcenroe@gmail.com>
2017-10-04 23:44:05 -0700

    Added start/stop interaction on images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,23 +11,23 @@ var topics = [
     'squash'
 ];
 
-$(document).ready(funciton(){
+$(document).ready(funciton() {
 
     createButtons(topics);
     createInput();
 });
 
-funciton createButtons(array) {
+funciton createButtons(array){
     $('body')
         .append($('<header>'));
 
     // Loops through array of buttons 
-    for (i = 0; i < array.length; i++) {
+    for (i = 0; i < array.length; i++){
         var button = "<button class='btn' id='" + array[i] + "'>" + array[i];
 
         $('header').append($(button)
             .text(array[i])
-            .on('click', function() {
+            .on('click', function(){
                 buttonClicked(this);
             })
 
@@ -36,12 +36,12 @@ funciton createButtons(array) {
     }
 }
 
-function newButton(query) {
+function newButton(query){
     var button = "<button class='btn' id='" + query + "'>" + query;
 
     $('header').append($(button)
         .text(query)
-        .on('click', function() {
+        .on('click', function(){
             buttonClicked(this);
         })
     );
@@ -55,7 +55,7 @@ function createInput() {
         .append($("<input type='text' class='form control' id='custom'>"))
         .append($("<input class='btn' id='searchBtn' type='submit' value='Search'>"));
 
-    $('label').text('Custom Search:');
+    $('label').text('Custom Search: ');
 
     $('searchBtn')
         .on('click', function() {
@@ -65,3 +65,70 @@ function createInput() {
         .text("Search");
 }
 
+function buttonClicked(Scope){
+  var clicked = $(scope).attr('id');
+
+  if (topics.indexOf(clicked) !== -1){
+  gifSearch(clicked);
+} else if (clicked === 'searchBtn'){
+  searchBtnClicked();
+  else {
+    console.log('Oops- something\'s not right')
+  }
+}
+
+function searchBtnClicked(){
+  var query = $('custom').val();
+
+  if (query === ''){
+    console.log('You must type something into the search box.')
+  } else {
+    topics.push(query)
+
+    newButton(query);
+
+    $('custom').val(' ');
+
+    gifSearch(query);
+  }
+}
+
+funciton gifSearch(searchTerm){
+  
+  //Reset the container html every time gifSearch runs
+  $('container').html("");
+
+  //Append a new container to the body
+  $('body').append($('<div class="main container">'));
+
+  var apiKey = "e9f95e9854974af3ac89fd3e68fa0759",
+    limit = 10,
+    rating = 'rating=pg';
+
+    var url = 'https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&' + rating + '&api_key=' + 
+    apiKey + '&limit' + limit;
+}
+
+//Animate and pause images on click
+$('img').on('click', function(){
+  var id = $(this).attr('id');
+
+  if ($(this).attr('static') === 'true'){
+    $(this).attr('src', data.data[id].images.fixed_width.url);
+    $(this).attr('static', 'false');
+  } else if {
+    $(this).attr('src', data.data[id].images.fixed_width.url);
+    $(this).attr("static", "true");
+  } else {
+    console.log('Oops- something\'s not right. Statci is: ' + 
+      $(this).attr('static'));
+  }
+  });
+
+
+  }
+}
+
+    }
+  }  
+}

f1d56183e231adbc12e47a0857a189c54024e4c1
Jarrod McEnroe <jwmcenroe@gmail.com>
2018-02-08 15:07:58 -0800

    Added npm files, switched to Bootstrap and added js functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,131 +1,86 @@
-var topics = [
-    'hot dog',
-    'hamburger',
-    'pizza',
-    'sushi',
-    'burrito',
-    'taco',
-    'chips',
-    'broccoli',
-    'tomato',
-    'squash'
-];
-
 $(document).ready(function() {
 
-    createButtons(topics);
-    createInput();
+    var searchGifs = {
+        searchTerms: ["helicopters", "cars", "planes", "lol", "jump", "fall", "laugh", "cute", "awwww"],
+        createButtons: function() {
+            for (var i = 0; i < searchGifs.searchTerms.length; i++) {
+                var newBttn = $('<button>');
+                newBttn.attr("data-search", searchGifs.searchTerms[i]);
+                newBttn.addClass("btn");
+                newBttn.addClass("searchButtons");
+                newBttn.text(searchGifs.searchTerms[i]);
+                $('#searchButtonsContainer').append(newBttn);
+            }
+        },
+        addSearchTerms: function(e) {
+          e.preventDefault();
+          var userTerm = $('#submitBox').val();
+
+          if (searchGifs.searchTerms.indexOf(userTerm) < 0 && userTerm.length > 0) {
+              searchGifs.searchTerms.push(userTerm);
+              var newBttn = $('<button>');
+              newBttn.attr("data-search", userTerm);
+              newBttn.addClass("btn");
+              newBttn.addClass("searchButtons");
+              newBttn.text(userTerm);
+              $('#searchButtonsContainer').append(newBttn);
+          }
+
+        },
+        displayResults: function(e) {
+            $('#showGIFS').empty();
+            e.preventDefault();
+
+            var userQuery = $(this).data('search');
+            var key = "&api_key=dc6zaTOxFJmzC";
+            var limit = "&limit=10"
+            var reqUrl = "https://api.giphy.com/v1/gifs/search?q=" + userQuery + limit + key;
+            // console.log(reqUrl);
+            $.ajax({
+                url: reqUrl,
+                method: "GET"
+            }).done(function(response) {
+
+                for (var i = 0; i < response.data.length; i++) {
+                    var gifContain = $('<div>');
+                    gifContain.addClass('gifContainer');
+                    var animateLink = response.data[i].images["fixed_height"].url;
+                    var stillLink = response.data[i].images["fixed_height_still"].url;
+                    var rating = response.data[i].rating;
+                    console.log(rating);
+                    var ratingSpan = $('<p>');
+                    ratingSpan.addClass('gifRating');
+                    ratingSpan.text("Rating: " + rating);
+                    var newImg = $('<img>');
+                    newImg.attr('src', stillLink);
+                    newImg.attr('data-animate', animateLink);
+                    newImg.attr('data-still', stillLink);
+                    newImg.attr('data-state', "still")
+                    newImg.addClass('gif');
+                    gifContain.prepend(ratingSpan);
+                    gifContain.append(newImg);
+
+                    $('#showGIFS').append(gifContain);
+                }
+
+                $('.gif').on("click", function() {
+                    var state = $(this).attr("data-state");
+                    if (state === "still") {
+                        $(this).attr('src', $(this).data("animate"));
+                        $(this).attr("data-state", "animate");
+                    } else {
+                        $(this).attr('src', $(this).data("still"));
+                        $(this).attr("data-state", "still");
+                    }
+                });
+            });
+        },
+
+    }
+    searchGifs.createButtons();
+
+
+
+    $('#submitTerm').click(searchGifs.addSearchTerms);
+    $(document).on('click', '.searchButtons', searchGifs.displayResults);
 });
-
-function createButtons(array) {
-    $('body')
-        .append($('<header>'));
-
-    // Loops through array of topics to create buttons 
-    for (i = 0; i < array.length; i++) {
-        var button = "<button class='btn' id='" + array[i] + "'>" + array[i];
-
-        $('header').append($(button)
-            .text(array[i])
-            .on('click', function() {
-                buttonClicked(this);
-            })
-        );
-    }
-}
-
-function newButton(query) {
-    var button = "<button class='btn' id='" + query + "'>" + query;
-
-    $("header")
-        .append(
-            $(button)
-            .text(query)
-            .on("click", function() {
-                buttonClicked(this);
-            })
-
-        );
-}
-
-function createInput() {
-    $('body').append($("<form class='search form'>"));
-
-    $('search form')
-        .append($("<label for='custom'>"))
-        .append($("<input type='text' class='form control' id='custom'>"))
-        .append($("<input class='btn' id='searchBtn' type='submit' value='Search'>"));
-
-    $('label').text('Custom Search: ');
-
-    $('searchBtn')
-        .on('click', function() {
-            event.preventDefault();
-            buttonClicked($(this));
-        })
-        .text("Search");
-}
-
-function buttonClicked(Scope) {
-    var clicked = $(scope).attr('id');
-
-    if (topics.indexOf(clicked) !== -1) {
-        gifSearch(clicked);
-    } else if (clicked === 'searchBtn') {
-        searchBtnClicked();
-    } else {
-        console.log('Oops- something\'s not right')
-    }
-}
-
-function searchBtnClicked() {
-    var query = $('custom').val();
-
-    if (query === '') {
-        console.log('You must type something into the search box.')
-    } else {
-        topics.push(query)
-
-        newButton(query);
-
-        $('custom').val(' ');
-
-        gifSearch(query);
-    }
-}
-
-function gifSearch(searchTerm) {
-
-    //Reset the container html every time gifSearch runs
-    $('container').html("");
-
-    //Append a new container to the body
-    $('body').append($('<div class="main container">'));
-
-    var apiKey = "e9f95e9854974af3ac89fd3e68fa0759",
-        limit = 10,
-        rating = 'rating=pg';
-
-    var url = 'https://api.giphy.com/v1/gifs/search?q=' + searchTerm + '&' + rating + '&api_key=' +
-        apiKey + '&limit' + limit;
-}
-
-//Animate and pause images on click
-$('img').on('click', function() {
-    var id = $(this).attr('id');
-
-    if ($(this).attr('static') === 'true') {
-        $(this).attr('src', data.data[id].images.fixed_width.url);
-        $(this).attr('static', 'false');
-    } else if {
-        $(this).attr('src', data.data[id].images.fixed_width.url);
-        $(this).attr("static", "true");
-    } else {
-        console.log('Oops- something\'s not right. Static is: ' +
-            $(this).attr('static'));
-    }
-});
-
-}
-}
-}
